bd82ca3108073bae309ab9b1a36c68657225944c
Patryk <patryk.bogdanski95@gmail.com>
2022-03-14 14:32:18 +0100

    Add swagger to Catalog API microservice

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -4,9 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+
+app.UseSwaggerUI(configuration =>
+{
+    configuration.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API"); // args: default path to swagger file, API docs name
+});
 
 app.UseAuthorization();
 
